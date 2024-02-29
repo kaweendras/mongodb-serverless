8377a62e432d2c708e3a1adbe79009677c3f1a43
kaweendras <salitha.wijerathna@gmail.com>
2024-02-29 11:12:54 +0530

    CHG|Get token ID from pathParams

--- functions/getNftByToken.js
+++ functions/getNftByToken.js
@@ -1,18 +1,21 @@
-exports = async function({ query }) {
+exports = async function ({ query }) {
   try {
     const mongodb = context.services.get("mongodb-atlas");
     const collection = mongodb.db("1155").collection("nft");
 
     // Extract the token_id from the request parameters
-    const token_id = query.tokenid;
+    const token_id = query.tokenid || context.request.pathParams.token;
 
     // Check if token_id is provided
     if (!token_id) {
-      return { error: 'Token ID is missing in the request parameters.' };
+      return { error: "Token ID is missing in the request parameters." };
     }
 
     // Execute a Find in MongoDB based on the provided token_id
-    const findResult = await collection.find({ token_id: token_id }).limit(100).toArray();
+    const findResult = await collection
+      .find({ token_id: token_id })
+      .limit(100)
+      .toArray();
 
     return { result: findResult };
   } catch (err) {
